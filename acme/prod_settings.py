59735f7516cf619aa41ad4fe634dc0560dd1c704
jack <jackthebestripper@yahoo.com>
2018-09-21 22:05:29 +0000

    Configure PostgreSQL

--- acme/prod_settings.py
+++ acme/prod_settings.py
@@ -1,7 +1,11 @@
+import dj_database_url
+
 from .settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = "i=9c8j#miy0b$e(*kn13s#=82w4nu7g7)11)!fm_+yuogtsw]@"
 
