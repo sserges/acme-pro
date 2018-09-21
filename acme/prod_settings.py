346c84c37270ef6deecc8eb5863fd00bfb51f668
jack <jackthebestripper@yahoo.com>
2018-09-21 22:41:27 +0000

    Setup whitenoise for staticfiles delivery

--- acme/prod_settings.py
+++ acme/prod_settings.py
@@ -12,3 +12,6 @@ SECRET_KEY = "x*(^3n(^)p%6vhig$l_#924qnw2jl-favui9mo2r4m4wx!wit("
 ALLOWED_HOSTS = [
     'acme-pro.herokuapp.com',
 ]
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
